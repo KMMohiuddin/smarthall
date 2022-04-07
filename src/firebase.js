3cd08e95e21eeabd7eb7c0f748e22581c35800c5
Mohiuddin <mohiuddin35-2894@diu.edu.bd>
2022-04-07 15:29:46 +0600

    firebase added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCsUGJdD2n2Hu85nDmWp1-cmrWjNwR-d9I",
+    authDomain: "smart-hall-aa20f.firebaseapp.com",
+    projectId: "smart-hall-aa20f",
+    storageBucket: "smart-hall-aa20f.appspot.com",
+    messagingSenderId: "724658587264",
+    appId: "1:724658587264:web:932b4528dffe0465db0865",
+    measurementId: "G-GHVJQLMPD3"
+  };
