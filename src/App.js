b503a380321bb56c0640909e40641d7e9d031da4
Mohiuddin <mohiuddin35-2894@diu.edu.bd>
2022-04-15 21:46:55 +0600

    major update1

--- src/App.js
+++ src/App.js
@@ -2,23 +2,30 @@ import React from 'react'
 import './App.css';
 import Header from './Header'
 import Sidebar from './Sidebar';
-
+import Feed from './Feed'
+import Widget from './Widget'
+import Login from './Login'
+import { useStateValue } from './StateProvider';
 
 function App() {
-  //const [{user}, dispatch] = useStateValue();
+  const [{user}, dispatch] = useStateValue();
   return (
     <div className="app">
-
-      <Header />
-      <h1>HELLO TEST 1</h1>
+      { !user ? (<Login />) : (
+        <>
+          <Header />
 
           <div className = "app__body">
-              <Sidebar />
+            <Sidebar />
     
+            <Feed />
+    
+            <Widget />
+            </div>
+        </>
+      )}
 
       
-            </div>
-
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,18 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase'
 const firebaseConfig = {
-    apiKey: "AIzaSyCsUGJdD2n2Hu85nDmWp1-cmrWjNwR-d9I",
-    authDomain: "smart-hall-aa20f.firebaseapp.com",
-    projectId: "smart-hall-aa20f",
-    storageBucket: "smart-hall-aa20f.appspot.com",
-    messagingSenderId: "724658587264",
-    appId: "1:724658587264:web:932b4528dffe0465db0865",
-    measurementId: "G-GHVJQLMPD3"
-  };
+  apiKey: "AIzaSyCsUGJdD2n2Hu85nDmWp1-cmrWjNwR-d9I",
+  authDomain: "smart-hall-aa20f.firebaseapp.com",
+  databaseURL: "https://smart-hall-aa20f-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "smart-hall-aa20f",
+  storageBucket: "smart-hall-aa20f.appspot.com",
+  messagingSenderId: "724658587264",
+  appId: "1:724658587264:web:54dc9fe37b5149bedb0865",
+  measurementId: "G-QXY9VPY61N"
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider} 
+export default db
